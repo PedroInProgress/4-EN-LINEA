14e154197dfda4977e9b0176ed6225f46b4b387e
PedroInProgress <55414280+PedroInProgress@users.noreply.github.com>
2019-11-08 01:36:28 -0300

    Version 8-11

---
+++ 4 en linea/src/juego/Aplicacion.java
@@ -0,0 +1,271 @@
+package juego;
+
+import java.nio.file.Paths;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ * Aplicación del juego Cuatro en Lí­nea.
+ * 
+ * Punto de entrada del programa.
+ * 
+ */
+public class Aplicacion extends Application {
+
+	public static final String TITULO = "Cuatro en Lí­nea";
+	
+	private GridPane grilla;
+	
+	private TextField campoNombreJugadorRojo;
+	private TextField campoNombreJugadorAmarillo;
+
+	private TextField campoColumnas;
+	private TextField campoFilas;
+
+	private Button botonIniciar;
+	
+	private boolean activado = true;
+	public int alMejorDe = 1;
+	
+	private Media sonidoBoton = new Media(Paths.get("./res/buenardo.mp3").toUri().toString());
+	
+	private Image imagenFondo = new Image(Paths.get("./res/fondo.jpg").toUri().toString());
+	
+	@Override
+	public void start(Stage escenarioPrincipal) {
+
+		crearGrilla();
+		
+		Scene escena = new Scene(grilla, 400, 300);
+
+		escenarioPrincipal.setScene(escena);
+		escenarioPrincipal.setTitle(TITULO);
+		escenarioPrincipal.show();
+		
+	}
+
+	private void crearGrilla() {
+
+		grilla = new GridPane();
+		grilla.setAlignment(Pos.CENTER);
+		grilla.setHgap(10);
+		grilla.setVgap(10);
+
+		Text textoTitulo = new Text(TITULO);
+		textoTitulo.setFont(new Font(16));
+		
+		crearControles();
+		
+		Button botonModoDeJuegoActivar = new Button("Activar");
+		Button botonModoDeJuegoDesactivar = new Button("Desactivar");
+
+		Label labelBotonModoDeJuego = new Label();
+		labelBotonModoDeJuego.setText("Activar modo varias\nfilas (DESACTIVADO)");
+		
+		Button botonAlMejorDeUno = new Button("  1  ");
+		Button botonAlMejorDeTres = new Button("  3  ");
+		Button botonAlMejorDeCinco = new Button("  5  ");
+		
+		Label labelbotonAlMejorDe = new Label();
+		labelbotonAlMejorDe.setText("Jugar al mejor de: 1");
+
+		BackgroundImage fondo = new BackgroundImage(imagenFondo,BackgroundRepeat.NO_REPEAT,
+				BackgroundRepeat.NO_REPEAT,
+				BackgroundPosition.DEFAULT,
+				new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, true, true));
+		
+		grilla.setBackground(new Background(fondo));
+		
+		grilla.add(textoTitulo, 0, 0, 2, 1);
+		grilla.add(new Label("Jugador Rojo"), 0, 1);
+		grilla.add(campoNombreJugadorRojo, 1, 1);
+		grilla.add(new Label("Jugador Amarillo"), 0, 2);
+		grilla.add(campoNombreJugadorAmarillo, 1, 2);
+		grilla.add(new Label("Filas (8 maximo)"), 0, 3);
+		grilla.add(campoFilas, 1, 3);
+		grilla.add(new Label("Columnas (16 maximo)"), 0, 4);
+		grilla.add(campoColumnas, 1, 4);
+		grilla.add(labelBotonModoDeJuego,0,5);
+		grilla.add(botonModoDeJuegoActivar, 1, 5, 1, 1);
+		grilla.add(botonModoDeJuegoDesactivar, 1, 5, 1, 1);
+		grilla.add(labelbotonAlMejorDe,0,6);
+		grilla.add(botonAlMejorDeUno, 1, 6, 1, 1);
+		grilla.add(botonAlMejorDeTres, 1, 6, 1, 1);
+		grilla.add(botonAlMejorDeCinco, 1, 6, 1, 1);
+		grilla.add(botonIniciar, 0, 7, 2, 1);
+		
+		
+//		grilla.setGridLinesVisible(true);
+		GridPane.setHalignment(botonAlMejorDeUno, HPos.LEFT);
+		GridPane.setHalignment(botonAlMejorDeTres, HPos.CENTER);
+		GridPane.setHalignment(botonAlMejorDeCinco, HPos.RIGHT);
+
+		GridPane.setHalignment(botonModoDeJuegoActivar, HPos.LEFT);
+		GridPane.setHalignment(botonModoDeJuegoDesactivar, HPos.RIGHT);
+		GridPane.setHalignment(botonIniciar, HPos.CENTER);
+		GridPane.setHalignment(textoTitulo, HPos.CENTER);
+		
+		
+		
+//		grilla.styleProperty().set("-fx-background-color: #C5EFE2");
+		
+		botonModoDeJuegoActivar.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				activado = true;
+				
+				if(activado)	labelBotonModoDeJuego.setText("Activar modo varias\nfilas (ACTIVADO)");
+
+				else	labelBotonModoDeJuego.setText("Activar modo varias\nfilas (DESACTIVADO)");
+				
+				reproducir();
+			}
+		});
+		
+		botonModoDeJuegoDesactivar.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				
+				activado = false;
+				
+				if(activado)	labelBotonModoDeJuego.setText("Activar modo varias\nfilas (ACTIVADO)");
+
+				else	labelBotonModoDeJuego.setText("Activar modo varias\nfilas (DESACTIVADO)");
+				
+				reproducir();
+			
+			}
+		});
+		
+		botonAlMejorDeUno.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				alMejorDe = 1;
+				
+				if(alMejorDe == 1)	labelbotonAlMejorDe.setText("Jugar al mejor de: 1");
+				
+				else if(alMejorDe == 3)	labelbotonAlMejorDe.setText("Jugar al mejor de: 3");
+				
+				else if(alMejorDe == 5)	labelbotonAlMejorDe.setText("Jugar al mejor de: 5");
+				
+				reproducir();
+
+			}
+		});
+		
+		botonAlMejorDeTres.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				alMejorDe = 3;
+				
+				if(alMejorDe == 1)	labelbotonAlMejorDe.setText("Jugar al mejor de: 1");
+				
+				else if(alMejorDe == 3)	labelbotonAlMejorDe.setText("Jugar al mejor de: 3");
+				
+				else if(alMejorDe == 5)	labelbotonAlMejorDe.setText("Jugar al mejor de: 5");
+				
+				reproducir();
+			}
+		});
+		
+		botonAlMejorDeCinco.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				alMejorDe = 5;
+				
+				if(alMejorDe == 1)	labelbotonAlMejorDe.setText("Jugar al mejor de: 1");
+				
+				else if(alMejorDe == 3)	labelbotonAlMejorDe.setText("Jugar al mejor de: 3");
+				
+				else if(alMejorDe == 5)	labelbotonAlMejorDe.setText("Jugar al mejor de: 5");
+				
+				reproducir();
+			}
+		});
+	}
+
+	private void reproducir(){
+			
+		MediaPlayer mediaPlayer = new MediaPlayer(sonidoBoton);
+
+		mediaPlayer.setVolume(1);
+		
+		mediaPlayer.play();
+	}
+	
+	private void crearControles() {
+
+		campoNombreJugadorRojo = new TextField("Jugador 1");
+		campoNombreJugadorAmarillo = new TextField("Jugador 2");
+		
+		campoColumnas = new TextField("4");		
+		
+		campoFilas = new TextField("4");
+		
+		botonIniciar = new Button("Iniciar");
+		
+		botonIniciar.setOnAction(new IniciarJuego(this));
+	}
+	
+	/**
+	 * post: crea un juego CuatroEnLinea, lo asocia a una Tablero 
+	 * 		 y comienza su ejecución.
+	 * 
+	 */
+	public void iniciar() {
+		
+		String nombreJugadorRojo = campoNombreJugadorRojo.getText();
+		String nombreJugadorAmarillo = campoNombreJugadorAmarillo.getText();
+		int filas = Integer.parseInt(campoFilas.getText());
+		int columnas = Integer.parseInt(campoColumnas.getText());
+		
+		CuatroEnLinea juego = new CuatroEnLinea(filas, columnas, 
+												nombreJugadorRojo, nombreJugadorAmarillo);
+		
+		Tablero tablero = new Tablero(juego);
+		tablero.mostrar();
+	}
+	
+	/**
+	 * post: inicializa la aplicacion y posteriormente llama al metodo start()
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+}
---
+++ 4 en linea/src/juego/Tablero.java
@@ -0,0 +1,281 @@
+package juego;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+
+
+
+
+
+
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.AudioClip;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.PopupWindow;
+import javafx.stage.Stage;
+import javafx.application.Application;  
+import javafx.scene.Group;  
+
+/**
+ * Representación gráfica del Tablero del Juego Cuatro en Lí­nea.
+ * 
+ */
+public class Tablero {
+
+	private static final int ALTO_FILA = 80;
+	private static final int ANCHO_COLUMNA = 80;
+	private static final int ALTURA_BOTON = 40;
+	private static final double RADIO = Math.min(ALTO_FILA - 1, ANCHO_COLUMNA - 1) / 2;
+	
+	private Image imagenFondo = new Image(Paths.get("./res/fondo.jpg").toUri().toString());
+	
+	private CuatroEnLinea juego;
+	private GridPane grilla;
+	private Stage escenario;
+
+	/**
+	 * post: asocia el Tablero a 'nuevoJuego' y lo inicializa a partir de su estado. 
+	 * 
+	 * @param nuevoJuego
+	 */
+	public Tablero(CuatroEnLinea nuevoJuego) {
+		
+		juego = nuevoJuego;
+		escenario = new Stage();
+		grilla = new GridPane();
+	
+	}
+	
+	/**
+	 * post: muestra el Tablero en pantalla.
+	 */
+	public void mostrar() {
+		
+		dibujarBotones();
+		
+//		grilla.setHgap(4);
+//		grilla.setVgap(4);
+		
+		grilla.setAlignment(Pos.CENTER);
+		
+		double ancho = juego.contarColumnas() * ANCHO_COLUMNA;
+		double alto = (juego.contarFilas() * ALTO_FILA) + ALTURA_BOTON;
+		
+		Scene escena = new Scene(grilla, ancho, alto);
+		
+//		grilla.setGridLinesVisible(true);
+		
+		escenario.setScene(escena);
+		escenario.setResizable(false);
+		escenario.setTitle(Aplicacion.TITULO);
+			
+		grilla.styleProperty().set("-fx-background-color: #0003E0");
+		
+		dibujar();
+
+		escenario.show();
+	}
+	
+	/**
+	 * post: agrega los botones para soltar una ficha en cada columna del Tablero.
+	 */
+	
+	/*
+	 * Con este metodo 
+	 */
+	private void dibujarBotones() {
+		
+		for (int columna = 1; columna <= juego.contarColumnas(); columna++) {
+
+			Button botonSoltarFicha = new Button("soltar");
+			botonSoltarFicha.setMinHeight(ALTURA_BOTON);
+
+			botonSoltarFicha.setOnAction(new SoltarFicha(this, juego, columna));
+			botonSoltarFicha.setMinWidth(ANCHO_COLUMNA);
+			grilla.add(botonSoltarFicha, columna - 1, 0);
+		}
+	}
+	
+	/**
+	 * post: actualiza el Tablero a partir del estado del juego asociado.
+	 */
+	public void dibujar() {
+
+		for (int fila = 1; fila <= juego.contarFilas(); fila++) {
+
+			for (int columna = 1; columna <= juego.contarColumnas(); columna++) {
+
+				Casillero casillero = juego.obtenerCasillero(fila, columna);
+				
+				Circle dibujoCasillero = dibujarCasillero(casillero);
+				
+				grilla.add(dibujoCasillero, columna - 1, fila);
+			}
+		}
+	}
+
+	/**
+	 * post: dibuja y devuelve el casillero dado.
+	 * 
+	 * 
+	 * @param casillero
+	 * @return representación gráfica del Casillero.
+	 */
+	private Circle dibujarCasillero(Casillero casillero) {
+		
+		Circle dibujoCasillero = new Circle(RADIO, obtenerPintura(casillero));
+		
+		dibujoCasillero.setStroke(new Color(0.5, 0.5, 0.5, 1.0));
+		dibujoCasillero.setScaleX(0.95);
+		dibujoCasillero.setScaleY(0.95);
+//		dibujoCasillero.setEffect(new DropShadow(+25d, 0d, +2d, Color.DARKSEAGREEN));
+		dibujoCasillero.setFill(new ImagePattern(imagenFondo));
+		return dibujoCasillero;
+	}
+
+	/**
+	 * post: determina la pintura a utilizar para 'casillero'.
+
+	 * @param casillero
+	 * @return pintura a utilizar para identificar el Casillero.
+	 */
+	private Paint obtenerPintura(Casillero casillero) {
+
+		Paint pintura;
+
+		switch (casillero) {
+		
+			case AMARILLO:
+				pintura = Color.YELLOW;
+				break;
+				
+			case ROJO:
+				pintura = Color.RED;
+				break;
+			
+			case VERDE:
+				pintura = Color.GREEN;
+				break;
+				
+			default:
+				pintura = Color.WHITE;
+		}
+
+		return pintura;
+	}
+
+	/**
+	 * pre : el juego asociado terminó.
+	 * post: muestra un mensaje indicando el resultado del juego y pregunta si se quiere
+	 * 		 volver a empezar la partida o salir..
+	 */
+	public void mostrarResultado() {
+
+		Stage dialogo = new Stage();
+		
+		Button botonJugarDeNuevo = new Button("Si");
+		Button botonSalirDelJuego = new Button("Salir");
+		
+		int ancho = 41; 
+	
+		botonSalirDelJuego.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				((Stage)(((Button)arg0.getSource()).getScene().getWindow())).close();
+				Platform.exit();
+			}
+		});
+		
+		botonJugarDeNuevo.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				reiniciarTablero();
+				((Stage)(((Button)arg0.getSource()).getScene().getWindow())).close();
+			}
+		});
+		
+
+		
+		Text textoJugarDeNuevo = new Text();
+		Text textoResultado = new Text();
+		Font fuente = new Font(28.0);
+		
+		textoJugarDeNuevo.setText("Jugar de nuevo?");
+		textoResultado.setText("Ganó el jugador: Mariano Tugnarelli");
+		
+		if (juego.hayGanador()) {
+		
+			textoResultado = new Text("Ganó el jugador: " + juego.obtenerGanador());
+			
+		} else {
+			
+			textoResultado = new Text("Empataron");
+		}
+		
+		GridPane grilla = new GridPane();
+
+		grilla.setAlignment(Pos.CENTER);
+//		grilla.setHgap(0);
+//		grilla.setVgap(10);
+		//
+		grilla.setGridLinesVisible(true);
+		//
+		grilla.getRowConstraints().add(new RowConstraints(ancho));
+		grilla.getRowConstraints().add(new RowConstraints(ancho));
+		grilla.getRowConstraints().add(new RowConstraints(ancho));
+
+		grilla.getColumnConstraints().add(new ColumnConstraints(ancho));
+		grilla.getColumnConstraints().add(new ColumnConstraints(ancho));
+		grilla.getColumnConstraints().add(new ColumnConstraints(ancho));
+
+		grilla.add(botonJugarDeNuevo,0,2,1,1);
+		grilla.add(botonSalirDelJuego,2,2,1,1);
+		grilla.add(textoJugarDeNuevo,0,1,3,1);
+		grilla.add(textoResultado,0,0,3,1);
+		
+		GridPane.setHalignment(botonJugarDeNuevo, HPos.CENTER);
+		GridPane.setHalignment(botonSalirDelJuego, HPos.CENTER);
+		GridPane.setHalignment(textoResultado, HPos.CENTER);
+		GridPane.setHalignment(textoJugarDeNuevo, HPos.CENTER);
+	
+		textoResultado.setFont(fuente);
+		textoJugarDeNuevo.setFont(fuente);
+
+		Scene escenaGanador = new Scene(grilla,400,200);
+
+		dialogo.setScene(escenaGanador);
+		dialogo.show();
+	}
+	
+	public void reiniciarTablero(){
+		
+		juego.reiniciarJuego();
+		dibujar();
+	}
+}
